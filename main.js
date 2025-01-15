9b3518aa2012ff4293e6328ecd10571b404d20f9
naeyeon Kang <ganggom2@gmail.com>
2025-01-15 17:17:48 +0900

    feat: closed #1

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const num2 = 20;
 
 function addNums(num1, num2){
     console.log(`${num1}+${num2} = ${num1+num2}`);
-    console.log("기능 완료");
+    console.log("기능 완료...");
 }
 
 // 코드 러너 실행 Ctrl + Alt + N
